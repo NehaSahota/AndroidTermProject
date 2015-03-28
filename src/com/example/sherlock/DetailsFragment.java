e82bdd7b65a0bb9cb381bc4b2be5575e1e345b20
Bino Oommen Samuel <binooommen37@gmail.com>
2015-03-27 20:22:22 -0400

    all layout changes and functions changed

---
+++ src/com/example/sherlock/DetailsFragment.java
@@ -0,0 +1,252 @@
+package com.example.sherlock;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailsFragment extends Fragment
+{
+   // callback methods implemented by MainActivity  
+   public interface DetailsFragmentListener
+   {
+      // called when a Mission is deleted
+      public void onMissionDeleted();
+      
+      // called to pass Bundle of Mission's info for editing
+      public void onEditMission(Bundle arguments);
+   }
+   
+   private DetailsFragmentListener listener;
+   
+   private long rowID = -1; // selected Mission's rowID
+   private TextView nameTextView; // displays Mission's name 
+   private TextView heightTextView; // displays Mission's phone
+   // private TextView writerTextView; // displays Mission's email
+   private TextView genderTextView; // displays Mission's street
+   private TextView haircolorTextView; // displays Mission's city
+   private TextView ageTextView; // displays Mission's state
+   private TextView notesTextView; // displays Mission's zip
+   
+   // set DetailsFragmentListener when fragment attached   
+   @Override
+   public void onAttach(Activity activity)
+   {
+      super.onAttach(activity);
+      listener = (DetailsFragmentListener) activity;
+   }
+   
+   // remove DetailsFragmentListener when fragment detached
+   @Override
+   public void onDetach()
+   {
+      super.onDetach();
+      listener = null;
+   }
+
+   // called when DetailsFragmentListener's view needs to be created
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState)
+   {
+      super.onCreateView(inflater, container, savedInstanceState);  
+      setRetainInstance(true); // save fragment across config changes
+
+      // if DetailsFragment is being restored, get saved row ID
+      if (savedInstanceState != null) 
+         rowID = savedInstanceState.getLong(MainActivity.ROW_ID);
+      else 
+      {
+         // get Bundle of arguments then extract the Mission's row ID
+         Bundle arguments = getArguments(); 
+         
+         if (arguments != null)
+            rowID = arguments.getLong(MainActivity.ROW_ID);
+      }
+         
+      // inflate DetailsFragment's layout
+      View view = 
+         inflater.inflate(R.layout.fragment_details, container, false);               
+      setHasOptionsMenu(true); // this fragment has menu items to display
+
+      // get the EditTexts
+      nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+      heightTextView = (TextView) view.findViewById(R.id.heightTextView);
+      // writerTextView = (TextView) view.findViewById(R.id.producerTextView);
+      genderTextView = (TextView) view.findViewById(R.id.genderTextView);
+      haircolorTextView = (TextView) view.findViewById(R.id.haircolorTextView);
+      ageTextView = (TextView) view.findViewById(R.id.ageTextView);
+      notesTextView = (TextView) view.findViewById(R.id.notesTextView);
+      return view;
+   }
+   
+   // called when the DetailsFragment resumes
+   @Override
+   public void onResume()
+   {
+      super.onResume();
+      new LoadMissionTask().execute(rowID); // load Mission at rowID
+   } 
+
+   // save currently displayed Mission's row ID
+   @Override
+   public void onSaveInstanceState(Bundle outState) 
+   {
+       super.onSaveInstanceState(outState);
+       outState.putLong(MainActivity.ROW_ID, rowID);
+   }
+
+   // display this fragment's menu items
+   @Override
+   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+   {
+      super.onCreateOptionsMenu(menu, inflater);
+      inflater.inflate(R.menu.fragment_details_menu, menu);
+   }
+
+   // handle menu item selections
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) 
+   {
+      switch (item.getItemId())
+      {
+         case R.id.action_edit: 
+            // create Bundle containing Mission data to edit
+            Bundle arguments = new Bundle();
+            arguments.putLong(MainActivity.ROW_ID, rowID);
+            arguments.putCharSequence("name", nameTextView.getText());
+            // arguments.putCharSequence("director", directorTextView.getText());
+            arguments.putCharSequence("height", heightTextView.getText());
+            arguments.putCharSequence("gender", genderTextView.getText());
+            arguments.putCharSequence("haircolor", haircolorTextView.getText());
+            arguments.putCharSequence("age", ageTextView.getText());
+            arguments.putCharSequence("notes", notesTextView.getText());            
+            listener.onEditMission(arguments); // pass Bundle to listener
+            return true;
+         case R.id.action_delete:
+            deleteMission();
+            return true;
+      }
+      
+      return super.onOptionsItemSelected(item);
+   } 
+   
+   // performs database query outside GUI thread
+   private class LoadMissionTask extends AsyncTask<Long, Object, Cursor> 
+   {
+      DBConnect databaseConnector = 
+         new DBConnect(getActivity());
+
+      // open database & get Cursor representing specified Mission's data
+      @Override
+      protected Cursor doInBackground(Long... params)
+      {
+         databaseConnector.open();
+         return databaseConnector.getOneMission(params[0]);
+      } 
+
+      // use the Cursor returned from the doInBackground method
+      @Override
+      protected void onPostExecute(Cursor result)
+      {
+         super.onPostExecute(result);
+         result.moveToFirst(); // move to the first item 
+   
+         // get the column index for each data item
+         int nameIndex = result.getColumnIndex("name");
+         // int directorIndex = result.getColumnIndex("director");
+         int heightIndex = result.getColumnIndex("height");
+         int genderIndex = result.getColumnIndex("gender");
+         int haircolorIndex = result.getColumnIndex("haircolor");
+         int ageIndex = result.getColumnIndex("age");
+         int notesIndex = result.getColumnIndex("notes");
+   
+         // fill TextViews with the retrieved data
+         nameTextView.setText(result.getString(nameIndex));
+         // directorTextView.setText(result.getString(directorIndex));
+         heightTextView.setText(result.getString(heightIndex));
+         genderTextView.setText(result.getString(genderIndex));
+         haircolorTextView.setText(result.getString(haircolorIndex));
+         ageTextView.setText(result.getString(ageIndex));
+         notesTextView.setText(result.getString(notesIndex));
+   
+         result.close(); // close the result cursor
+         databaseConnector.close(); // close database connection
+      } // end method onPostExecute
+   } // end class LoadMissionTask
+
+   // delete a Mission
+   private void deleteMission()
+   {         
+      // use FragmentManager to display the confirmDelete DialogFragment
+      confirmDelete.show(getFragmentManager(), "confirm delete");
+   } 
+
+   // DialogFragment to confirm deletion of Mission
+   private DialogFragment confirmDelete = 
+      new DialogFragment()
+      {
+         // create an AlertDialog and return it
+         @Override
+         public Dialog onCreateDialog(Bundle bundle)
+         {
+            // create a new AlertDialog Builder
+            AlertDialog.Builder builder = 
+               new AlertDialog.Builder(getActivity());
+      
+            builder.setTitle(R.string.confirm_title); 
+            builder.setMessage(R.string.confirm_message);
+      
+            // provide an OK button that simply dismisses the dialog
+            builder.setPositiveButton(R.string.button_delete,
+               new DialogInterface.OnClickListener()
+               {
+                  @Override
+                  public void onClick(
+                     DialogInterface dialog, int button)
+                  {
+                     final DBConnect databaseConnector = 
+                        new DBConnect(getActivity());
+      
+                     // AsyncTask deletes Mission and notifies listener
+                     AsyncTask<Long, Object, Object> deleteTask =
+                        new AsyncTask<Long, Object, Object>()
+                        {
+                           @Override
+                           protected Object doInBackground(Long... params)
+                           {
+                              databaseConnector.deleteMission(params[0]); 
+                              return null;
+                           } 
+      
+                           @Override
+                           protected void onPostExecute(Object result)
+                           {                                 
+                              listener.onMissionDeleted();
+                           }
+                        }; // end new AsyncTask
+      
+                     // execute the AsyncTask to delete Mission at rowID
+                     deleteTask.execute(new Long[] { rowID });               
+                  } // end method onClick
+               } // end anonymous inner class
+            ); // end call to method setPositiveButton
+            
+            builder.setNegativeButton(R.string.button_cancel, null);
+            return builder.create(); // return the AlertDialog
+         }
+      }; // end DialogFragment anonymous inner class
+} // end class DetailsFragment
+
--- src/com/example/sherlock/MainActivity.java
+++ src/com/example/sherlock/MainActivity.java
@@ -2,40 +2,152 @@ package com.example.sherlock;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
+import android.app.FragmentTransaction;
 
+public class MainActivity extends Activity 
+   implements MissionListFragment.MissionListFragmentListener,
+      DetailsFragment.DetailsFragmentListener, 
+      AddEditDetailsFragment.AddEditFragmentListener
+{
+   // keys for storing row ID in Bundle passed to a fragment
+   public static final String ROW_ID = "row_id"; 
+   
+   MissionListFragment missionListFragment; // displays mission list
+   
+   // display missionListFragment when MainActivity first loads
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
 
-public class MainActivity extends Activity {
+      // return if Activity is being restored, no need to recreate GUI
+      if (savedInstanceState != null) 
+         return;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); 
-    }
-        
-    
-    
+      // check whether layout contains fragmentContainer (phone layout);
+      // missionListFragment is always displayed
+      if (findViewById(R.id.fragmentContainer) != null) 
+      {
+         // create missionListFragment
+         missionListFragment = new MissionListFragment();
+         
+         // add the fragment to the FrameLayout
+         FragmentTransaction transaction = 
+            getFragmentManager().beginTransaction();
+         transaction.add(R.id.fragmentContainer, missionListFragment);
+         transaction.commit(); // causes missionListFragment to display
+      }
+   }
+   
+   // called when MainActivity resumes
+   @Override
+   protected void onResume()
+   {
+      super.onResume();
+      
+      // if missionListFragment is null, activity running on tablet, 
+      // so get reference from FragmentManager
+      if (missionListFragment == null)
+      {
+         missionListFragment = 
+            (MissionListFragment) getFragmentManager().findFragmentById(
+               R.id.missionListFragment);      
+      }
+   }
+   
+   // display DetailsFragment for selected mission
+   @Override
+   public void onMissionSelected(long rowID)
+   {
+      if (findViewById(R.id.fragmentContainer) != null) // phone
+         displayMission(rowID, R.id.fragmentContainer);
+      else // tablet
+      {
+         getFragmentManager().popBackStack(); // removes top of back stack
+         displayMission(rowID, R.id.rightPaneContainer);
+      }
+   }
 
+   // display a mission
+   private void displayMission(long rowID, int viewID)
+   {
+      DetailsFragment detailsFragment = new DetailsFragment();
+      
+      // specify rowID as an argument to the DetailsFragment
+      Bundle arguments = new Bundle();
+      arguments.putLong(ROW_ID, rowID);
+      detailsFragment.setArguments(arguments);
+      
+      // use a FragmentTransaction to display the DetailsFragment
+      FragmentTransaction transaction = 
+         getFragmentManager().beginTransaction();
+      transaction.replace(viewID, detailsFragment);
+      transaction.addToBackStack(null);
+      transaction.commit(); // causes DetailsFragment to display
+   }
+   
+   // display the AddEditFragment to add a new mission
+   @Override
+   public void onAddMission()
+   {
+      if (findViewById(R.id.fragmentContainer) != null)
+         displayAddEditFragment(R.id.fragmentContainer, null); 
+      else
+         displayAddEditFragment(R.id.rightPaneContainer, null);
+   }
+   
+   // display fragment for adding a new or editing an existing mission
+   private void displayAddEditFragment(int viewID, Bundle arguments)
+   {
+      AddEditDetailsFragment addEditFragment = new AddEditDetailsFragment();
+      
+      if (arguments != null) // editing existing mission
+         addEditFragment.setArguments(arguments);
+      
+      // use a FragmentTransaction to display the AddEditFragment
+      FragmentTransaction transaction = 
+         getFragmentManager().beginTransaction();
+      transaction.replace(viewID, addEditFragment);
+      transaction.addToBackStack(null);
+      transaction.commit(); // causes AddEditFragment to display
+   }
+   
+   // return to mission list when displayed mission deleted
+   @Override
+   public void onMissionDeleted()
+   {
+      getFragmentManager().popBackStack(); // removes top of back stack
+      
+      if (findViewById(R.id.fragmentContainer) == null) // tablet
+         missionListFragment.updateMissionList();
+   }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+   // display the AddEditFragment to edit an existing mission
+   @Override
+   public void onEditMission(Bundle arguments)
+   {
+      if (findViewById(R.id.fragmentContainer) != null) // phone
+         displayAddEditFragment(R.id.fragmentContainer, arguments); 
+      else // tablet
+         displayAddEditFragment(R.id.rightPaneContainer, arguments);
+   }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+   // update GUI after new mission or updated mission saved
+   @Override
+   public void onAddEditCompleted(long rowID)
+   {
+      getFragmentManager().popBackStack(); // removes top of back stack
+
+      if (findViewById(R.id.fragmentContainer) == null) // tablet
+      {
+         getFragmentManager().popBackStack(); // removes top of back stack
+         missionListFragment.updateMissionList(); // refresh missions
+
+         // on tablet, display mission that was just added or edited
+         displayMission(rowID, R.id.rightPaneContainer); 
+      }
+   }   
 }
+
+
